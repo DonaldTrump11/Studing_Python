a82e4f3d22e56dd8e4778ec38c30b10a08411596
DonaldTrump11 <yellowsubmarine555555@rambler.ru>
2023-05-17 11:27:59 +0300

    Задание решено

--- Second_week/Additional_tasks/Exercise_1.py
+++ Second_week/Additional_tasks/Exercise_1.py
@@ -1,3 +1,21 @@
+def gen_series(series):
+    """
+    генератор серий лотерейных билетов, входные параметры: series -  - номер серии,
+    выход - строка, состоящая из двух заглавных букв латинского алфавита
+    """
+    a=0
+    for i in range(ord(series[0].lower()), 123):
+        if a ==0 :
+            a = 1
+            for j in range(ord(series[1].lower()), 123) :
+                s = (chr(i)+chr(j)).upper()
+                yield s
+        else: 
+            for j in range(97,123) :
+                s = (chr(i)+chr(j)).upper()
+                yield s
+
+
 def gen_number(length=6):
     """
     генератор номеров лотерейных билетов в одной серии, входные параметры:
@@ -5,31 +23,8 @@ def gen_number(length=6):
     """
     all_tickets = []
     for i in range(1, 10**length) :
-        all_tickets.append(str(i).zfill(length))
-    return all_tickets
-    pass
-
-
-def gen_series(series):
-    """
-    генератор серий лотерейных билетов, входные параметры: series -  - номер серии,
-    выход - строка, состоящая из двух заглавных букв латинского алфавита
-    """
-    all_series = []
-    for i in range(ord(series[0].lower()), 123) :
-        for j in range(97, 123):
-            all_series.append((chr(i)+chr(j)).upper())
-
-    x = 0;
-    while x <= len(all_series):
-        if all_series[x] == series.upper():
-            break;
-        else:
-            all_series.pop(x)
-
-    return all_series
-    pass
-
+        yield(str(i).zfill(length))
+        
 
 def gen_ticket_number(count, series, length=6):
     """
@@ -37,19 +32,15 @@ def gen_ticket_number(count, series, length=6):
     series - номер серии, необязательный аргумент length - количество цифр
     в номере, по умолчанию равен 6, выход - строка вида: <номер билета> <серия билета>
     """
-    all_tickets = []
-    j = 0
-
-    num = 0
-    while num < count:
-        if num >= (10 ** length):
-            j = j + 1
-            num = 0
-            count = count - (10 ** length - 1);
-        else :
-            all_tickets.append([str(num+1).zfill(length), gen_series(series)[j]])
-        num += 1
-
-
-    return all_tickets
-    pass
+             
+    gener_series = gen_series(series)
+    ser = next(gener_series)
+    i = 0;
+    while i < count :
+        i +=1
+        if i >= 10**length :
+            i = i - (10**length-1)
+            count = count- 10**length
+            ser = next(gener_series)
+        answer = str(i).zfill(length) + " " + ser
+        yield answer
